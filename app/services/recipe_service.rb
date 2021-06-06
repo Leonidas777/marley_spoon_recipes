7735e9de3dca5229b781565f248f3a944b968925
Leonid Smirnov <leonid.smirnov@uchi.ru>
2021-06-06 15:08:59 +0300

    Added the initializing the Contentful client instance in the Recipe service.

--- app/services/recipe_service.rb
+++ app/services/recipe_service.rb
@@ -5,5 +5,14 @@ class RecipeService
 
     def get
     end
+
+    private
+
+    def contentful_client
+      @contentful_client ||= Contentful::Client.new(
+        space: ENV['CONTENTFUL_SPACE_ID'],
+        access_token: ENV['CONTENTFUL_ACCESS_TOKEN']
+      )
+    end
   end
 end
