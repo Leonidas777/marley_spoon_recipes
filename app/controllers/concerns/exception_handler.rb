5f8839291cbc8f2ab32f9e114bd224826d9488dd
Leonid Smirnov <leonid.smirnov@uchi.ru>
2021-06-06 20:18:16 +0300

    Fixed the concern.

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,27 +1,29 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
 
-  rescue_from StandardError do |e|
-    render 'errors/500', status: 500
-  end
+  included do
+    rescue_from StandardError do |e|
+      render 'errors/500', status: 500
+    end
 
-  rescue_from Contentful::BadRequest do |e|
-    render 'errors/400', status: 400
-  end
+    rescue_from Contentful::BadRequest do |e|
+      render 'errors/400', status: 400
+    end
 
-  rescue_from Contentful::Unauthorized do |e|
-    render 'errors/401', status: 401
-  end
+    rescue_from Contentful::Unauthorized do |e|
+      render 'errors/401', status: 401
+    end
 
-  rescue_from Contentful::AccessDenied do |e|
-    render 'errors/403', status: 403
-  end
+    rescue_from Contentful::AccessDenied do |e|
+      render 'errors/403', status: 403
+    end
 
-  rescue_from Contentful::NotFound do |e|
-    render 'errors/404', status: 404
-  end
+    rescue_from Contentful::NotFound do |e|
+      render 'errors/404', status: 404
+    end
 
-  rescue_from Contentful::RateLimitExceeded do |e|
-    render 'errors/429', status: 429
+    rescue_from Contentful::RateLimitExceeded do |e|
+      render 'errors/429', status: 429
+    end
   end
 end
