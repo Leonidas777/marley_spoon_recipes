bc9f3ed9a5c702b1874c6d7a6879d98f5f86dec9
Leonid Smirnov <leonid.smirnov@uchi.ru>
2021-06-06 20:15:10 +0300

    Put the logic of handling exceptions into a separate concern.

---
+++ app/controllers/concerns/exception_handler.rb
@@ -0,0 +1,27 @@
+module ExceptionHandler
+  extend ActiveSupport::Concern
+
+  rescue_from StandardError do |e|
+    render 'errors/500', status: 500
+  end
+
+  rescue_from Contentful::BadRequest do |e|
+    render 'errors/400', status: 400
+  end
+
+  rescue_from Contentful::Unauthorized do |e|
+    render 'errors/401', status: 401
+  end
+
+  rescue_from Contentful::AccessDenied do |e|
+    render 'errors/403', status: 403
+  end
+
+  rescue_from Contentful::NotFound do |e|
+    render 'errors/404', status: 404
+  end
+
+  rescue_from Contentful::RateLimitExceeded do |e|
+    render 'errors/429', status: 429
+  end
+end
