879dd126f35738ee92c066706a0c914877991189
Leonid Smirnov <leonid.smirnov@uchi.ru>
2021-06-06 14:37:28 +0300

    Added the main endpoints.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'recipes#index'
+
+  get 'recipes', to: 'recipes#index'
+  get 'recipe', to: 'recipes#show'
 end
