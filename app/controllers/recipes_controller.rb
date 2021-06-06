715aa7fc235e5cee92d7c12e0d0376cb5b8feca6
Leonid Smirnov <leonid.smirnov@uchi.ru>
2021-06-06 14:41:29 +0300

    Created the Recipe Service and added it to the controller's actions.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,9 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = RecipeService.get_all
   end
 
   def show
+    @recipe = RecipeService.get
   end
 end
---
+++ app/services/recipe_service.rb
@@ -0,0 +1,9 @@
+class RecipeService
+  class << self
+    def get_all
+    end
+
+    def get
+    end
+  end
+end
