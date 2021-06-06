5e45ed42d36da0e66137ee1901b018de98d2afaa
Leonid Smirnov <leonid.smirnov@uchi.ru>
2021-06-06 18:57:42 +0300

    Improved the test.

--- spec/services/recipe_service_spec.rb
+++ spec/services/recipe_service_spec.rb
@@ -20,12 +20,17 @@ describe RecipeService do
 
   describe '#get_by_id' do
     let(:id) { '5jyueR32224maKGqIOYW6' }
+
     subject { described_class.get_by_id(id) }
 
     it 'returns the recipe' do
       VCR.use_cassette('get_recipe') do
         result = subject
-        expect(result).to eq(nil)
+
+        expect(result.is_a?(Contentful::Entry)).to eq(true)
+        expect(result.title).to eq('Tofu Saag Paneer with Buttery Toasted Pita')
+        expect(result.photo.url.starts_with?('//images.ctfassets.net/')).to eq(true)
+        expect(result.description.start_with?('Saag paneer is a popular Indian dish')).to eq(true)
       end
     end
   end
